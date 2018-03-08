b5d4bd7d7ca53ed83d182cf4d0f59e17da9ec4b0
Thomas Jung <github@thomasjung.com>
2018-03-08 13:23:24 -0800

    fixing an illegal invocation error - need browser tests!

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var haveGlobal = (typeof global !== 'undefined');
-var now = haveGlobal && global.window && window.performance && performance.now || false;
+var np = haveGlobal && global.window && window.performance
+    && performance.now && performance || false;
 var nowMicro = global.process && process.hrtime
     ? function () {
         var hrTime = process.hrtime();
         return hrTime[0] * 1000000 + hrTime[1] / 1000;
     }
     // see https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
-    : now
-        ? function () { return now() * 1000; }
+    : np
+        ? function () { return np.now() * 1000; }
         : function () { return Date.now() * 1000; };
 exports.nowMicro = nowMicro;
 var Timer = /** @class */ (function () {
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,16 @@ declare namespace global {
 }
 
 const haveGlobal = (typeof global !== 'undefined')
-const now = haveGlobal && global.window && window.performance && performance.now || false
+const np = haveGlobal && global.window && window.performance
+        && performance.now && performance || false
 const nowMicro = global.process && process.hrtime
     ? () => {
       const hrTime = process.hrtime()
       return hrTime[0] * 1000000 + hrTime[1] / 1000
     }
     // see https://developer.mozilla.org/en-US/docs/Web/API/Performance/now
-    : now
-      ? () => now() * 1000
+    : np
+      ? () => np.now() * 1000
       : () => Date.now() * 1000
 
 export { nowMicro }
